a8e7ff60e99926f273153fbcacc9133033530d9b
David Anton <d.anton@tu-braunschweig.de>
2025-03-24 14:32:18 +0000

    Add io package

---
+++ bayesianmdisc/io/readerswriters/__init__.py
@@ -0,0 +1,13 @@
+from .csvdatareader import CSVDataReader
+from .dataclasswriter import DataclassWriter
+from .numpydatareader import NumpyDataReader
+from .numpydatawriter import NumpyDataWriter
+from .pandasdatawriter import PandasDataWriter
+
+__all__ = [
+    "CSVDataReader",
+    "DataclassWriter",
+    "NumpyDataReader",
+    "NumpyDataWriter",
+    "PandasDataWriter",
+]
