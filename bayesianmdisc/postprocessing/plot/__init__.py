6f0b2a42ac56e0d2f9edab2d13a729f7332dfd3f
David Anton <d.anton@tu-braunschweig.de>
2025-03-24 14:21:33 +0000

    Add postprocessing package

---
+++ bayesianmdisc/postprocessing/plot/__init__.py
@@ -0,0 +1,11 @@
+from .plot_history import (
+    HistoryPlotterConfig,
+    plot_loss_history,
+    plot_statistical_loss_history,
+)
+
+__all__ = [
+    "HistoryPlotterConfig",
+    "plot_loss_history",
+    "plot_statistical_loss_history",
+]
