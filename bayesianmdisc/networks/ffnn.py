c199fc4087ed07210567f0ce2b3b0ed697804dac
David Anton <d.anton@tu-braunschweig.de>
2025-03-26 17:36:08 +0000

    Add network package

---
+++ bayesianmdisc/networks/ffnn.py
@@ -0,0 +1,98 @@
+from typing import Callable, TypeAlias
+
+import torch
+import torch.nn as nn
+
+from bayesianmdisc.types import Module, Tensor
+
+InitializationFunc: TypeAlias = Callable[[Tensor], Tensor]
+Layers: TypeAlias = list[Module]
+
+
+class LinearHiddenLayer(nn.Module):
+    def __init__(
+        self,
+        size_input: int,
+        size_output: int,
+        activation: Module,
+        init_weights: InitializationFunc,
+        init_bias: InitializationFunc,
+    ) -> None:
+        super().__init__()
+        self._fc_layer = nn.Linear(
+            in_features=size_input,
+            out_features=size_output,
+            bias=True,
+        )
+        self._activation = activation
+        init_weights(self._fc_layer.weight)
+        init_bias(self._fc_layer.bias)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._activation(self._fc_layer(x))
+
+
+class LinearOutputLayer(nn.Module):
+    def __init__(
+        self,
+        size_input: int,
+        size_output: int,
+        init_weights: InitializationFunc,
+        init_bias: InitializationFunc,
+    ) -> None:
+        super().__init__()
+        self._fc_layer = nn.Linear(
+            in_features=size_input,
+            out_features=size_output,
+            bias=True,
+        )
+        init_weights(self._fc_layer.weight)
+        init_bias(self._fc_layer.bias)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._fc_layer(x)
+
+
+class FFNN(torch.nn.Module):
+    def __init__(
+        self,
+        layer_sizes: list[int],
+        activation=nn.Tanh(),
+        init_weights=nn.init.xavier_uniform_,
+        init_bias=nn.init.zeros_,
+    ) -> None:
+        super().__init__()
+        self._layers = self._set_up_layers(
+            layer_sizes, activation, init_weights, init_bias
+        )
+        self._output = nn.Sequential(*self._layers)
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        return self._output(inputs)
+
+    def _set_up_layers(
+        self,
+        layer_sizes: list[int],
+        activation: Module,
+        init_weights: InitializationFunc,
+        init_bias: InitializationFunc,
+    ) -> list[nn.Module]:
+        layers: list[nn.Module] = [
+            LinearHiddenLayer(
+                size_input=layer_sizes[i - 1],
+                size_output=layer_sizes[i],
+                activation=activation,
+                init_weights=init_weights,
+                init_bias=init_bias,
+            )
+            for i in range(1, len(layer_sizes) - 1)
+        ]
+
+        layer_out = LinearOutputLayer(
+            size_input=layer_sizes[-2],
+            size_output=layer_sizes[-1],
+            init_weights=init_weights,
+            init_bias=init_bias,
+        )
+        layers.append(layer_out)
+        return layers
