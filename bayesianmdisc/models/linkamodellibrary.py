b0faf791e43e9644d6587c1e8b9a473a13fe78d7
David Anton <d.anton@tu-braunschweig.de>
2025-03-27 15:21:06 +0000

    Renaming and bugfix

--- bayesianmdisc/models/linkamodellibrary.py
+++ bayesianmdisc/models/linkamodellibrary.py
@@ -172,7 +172,7 @@ from bayesianmdisc.types import Device, Tensor
 #     return deformation_gradient.reshape((3, 3))
 
 
-class LinkaOrthotropicIncompressibleCANN:
+class LinkaCANN:
 
     def __init__(self, device: Device):
         self.output_dim = 2
@@ -335,25 +335,25 @@ class LinkaOrthotropicIncompressibleCANN:
         row_1 = torch.concat(
             (
                 torch.unsqueeze(stretch_fiber, dim=0),
-                torch.tensor([0.0]),
-                torch.tensor([0.0]),
+                torch.tensor([0.0], device=self._device),
+                torch.tensor([0.0], device=self._device),
             )
         )
         row_2 = torch.concat(
             (
-                torch.tensor([0.0]),
+                torch.tensor([0.0], device=self._device),
                 torch.unsqueeze(stretch_sheet, dim=0),
-                torch.tensor([0.0]),
+                torch.tensor([0.0], device=self._device),
             )
         )
         row_3 = torch.concat(
             (
-                torch.tensor([0.0]),
-                torch.tensor([0.0]),
+                torch.tensor([0.0], device=self._device),
+                torch.tensor([0.0], device=self._device),
                 torch.unsqueeze(stretch_normal, dim=0),
             )
         )
-        return torch.stack((row_1, row_2, row_3)).to(self._device)
+        return torch.stack((row_1, row_2, row_3))
 
     def _split_parameters(self, parameters: Parameters) -> SplittedParameters:
         return torch.chunk(parameters, self._num_invariants)
@@ -377,3 +377,242 @@ class LinkaOrthotropicIncompressibleCANN:
                 torch.unsqueeze(stress_normal, dim=0),
             )
         )
+
+
+# class LinkaCANNLikelihood:
+#     def __init__(
+#         self,
+#         model: LinkaCANN,
+#         material_parameters: BayesianMaterialParameters,
+#         material_parameter_indices_func: MaterialParametersFunc,
+#         data_likelihood_parameters: DataLikelihoodParameters,
+#         pinn_funcs: PINNFuncs,
+#         training_dataset: TrainingDataset,
+#         calibration_dataset: ConcatenatedCalibrationData,
+#         error_calculator: ErrorCalculatorProtocol,
+#         error_distribution_creator: ErrorDistributionCreatorProtocol,
+#         device: Device,
+#     ) -> None:
+#         self._ansatz = model
+#         self._material_parameters = material_parameters
+#         self._material_parameter_indices_func = material_parameter_indices_func
+#         self._data_likelihood_parameters = data_likelihood_parameters
+#         self._pinn_funcs = pinn_funcs
+#         self._error_calculator = error_calculator
+#         self._error_distribution_creator = error_distribution_creator
+#         self._device = device
+#         self._data_pde, self._data_heatflux = self._unpack_training_dataset(
+#             training_dataset
+#         )
+#         self._data_calibration = calibration_dataset
+#         self._num_flattened_y_pde = len(self._data_pde.x_coor)
+#         self._num_flattened_y_heatflux = len(self._data_heatflux.y_true)
+#         self._num_flattened_y_data = len(self._data_calibration.outputs)
+#         (
+#             self._flattened_y_pde_true,
+#             self._flattened_y_heatflux_true,
+#             self._flattened_y_data_true,
+#         ) = self._assemble_flattened_true_outputs()
+#         self._parameter_indices_pde, self._parameter_indices_heatflux_bc = (
+#             self._assemble_parameter_indices()
+#         )
+#         self._num_likelihood_parameter_sets = 2
+#         self._likelihood_data = self._initialize_data_likelihood()
+#         self.num_material_params = self._material_parameters.num_parameters
+#         self.num_ansatz_params = self._ansatz.network.num_parameters
+#         self.likelihood_term_names = ["PDE", "HFBC", "Data"]
+
+#     def prob(self, parameters: Tensor) -> Prob:
+#         with torch.no_grad():
+#             return self._prob(parameters)
+
+#     def log_prob(self, parameters: Tensor) -> LogProb:
+#         with torch.no_grad():
+#             return self._log_prob(parameters)[0]
+
+#     def log_prob_with_grad(self, parameters: Tensor) -> LogProb:
+#         return self._log_prob(parameters)[0]
+
+#     def log_prob_and_individual_log_probs_with_grad(
+#         self, parameters: Tensor
+#     ) -> LogProbAndIndividualLogProbs:
+#         log_prob, individual_log_probs = self._log_prob(parameters)
+#         return log_prob, concat_zero_dimensional_tensors(individual_log_probs)
+
+#     def grad_log_prob(self, parameters: Tensor) -> GradLogProb:
+#         return torch.autograd.grad(
+#             self._log_prob(parameters)[0],
+#             parameters,
+#             retain_graph=True,
+#             create_graph=False,
+#         )[0]
+
+#     def mse_loss_statistics(self, parameters: Tensor) -> MSELossStatistics:
+#         mses_pde, mses_heatflux, mses_data = self._calculate_mse_losses(parameters)
+#         mean_mse_pde = torch.mean(mses_pde, dim=0).detach().cpu().item()
+#         std_mse_pde = torch.std(mses_pde, dim=0).detach().cpu().item()
+#         mean_mse_heatflux = torch.mean(mses_heatflux, dim=0).detach().cpu().item()
+#         std_mse_heatflux = torch.std(mses_heatflux, dim=0).detach().cpu().item()
+#         mean_mse_data = torch.mean(mses_data, dim=0).detach().cpu().item()
+#         std_mse_data = torch.std(mses_data, dim=0).detach().cpu().item()
+#         return MSELossStatistics(
+#             mean_pde=mean_mse_pde,
+#             std_pde=std_mse_pde,
+#             mean_heatflux_bc=mean_mse_heatflux,
+#             std_heatflux_bc=std_mse_heatflux,
+#             mean_data=mean_mse_data,
+#             std_data=std_mse_data,
+#         )
+
+#     def _unpack_training_dataset(
+#         self, training_dataset: TrainingDataset
+#     ) -> tuple[TrainingData2DCollocation, TrainingData2DHeatFluxBC]:
+#         training_dataloader = DataLoader(
+#             dataset=training_dataset,
+#             batch_size=len(training_dataset),
+#             collate_fn=training_dataset.get_collate_func(),
+#         )
+#         training_data_iter = iter(training_dataloader)
+#         data_pde, data_heatflux_bc = next(training_data_iter)
+#         return data_pde, data_heatflux_bc
+
+#     def _assemble_flattened_true_outputs(self) -> Outputs:
+#         y_pde_true = torch.zeros((self._num_flattened_y_pde,), device=self._device)
+#         y_heatflux_true = flatten_tensor(self._data_heatflux.y_true)
+#         y_data_true = flatten_tensor(self._data_calibration.outputs)
+#         return y_pde_true, y_heatflux_true, y_data_true
+
+#     def _assemble_parameter_indices(self) -> tuple[Tensor, Tensor]:
+#         parameter_indices_pde = torch.tensor(
+#             self._material_parameter_indices_func(self._data_pde.x_coor),
+#             dtype=torch.int,
+#             device=self._device,
+#         )
+#         parameter_indices_heatflux_bc = torch.tensor(
+#             self._material_parameter_indices_func(self._data_heatflux.x_coor),
+#             dtype=torch.int,
+#             device=self._device,
+#         )
+#         return parameter_indices_pde, parameter_indices_heatflux_bc
+
+#     def _prob(self, parameters: Tensor) -> Prob:
+#         return torch.exp(self._log_prob(parameters)[0])
+
+#     @abstractmethod
+#     def _log_prob(self, parameters: Tensor) -> LogProbAndIndividualLogProbsTuple:
+#         raise NotImplementedError()
+
+#     def _initialize_data_likelihood(self) -> Likelihood:
+#         likelihood_parameters = torch.tensor(
+#             [
+#                 self._data_likelihood_parameters.mean,
+#                 self._data_likelihood_parameters.std,
+#             ],
+#             device=self._device,
+#         )
+#         return self._error_distribution_creator.create(
+#             likelihood_parameters, total_num_errors=self._num_flattened_y_data
+#         )
+
+#     def _initialize_likelihoods(self, likelihood_parameters: Tensor) -> Likelihoods:
+#         parameters_pde, parameters_heatflux = self._split_likelihood_parameters(
+#             likelihood_parameters
+#         )
+#         likelihood_pde = self._error_distribution_creator.create(
+#             parameters_pde, total_num_errors=self._num_flattened_y_pde
+#         )
+#         likelihood_heatflux = self._error_distribution_creator.create(
+#             parameters_heatflux, total_num_errors=self._num_flattened_y_heatflux
+#         )
+#         return likelihood_pde, likelihood_heatflux
+
+#     def _split_likelihood_parameters(
+#         self, likelihood_parameters: Tensor
+#     ) -> LikelihoodParameterSets:
+#         return torch.chunk(
+#             likelihood_parameters, chunks=self._num_likelihood_parameter_sets
+#         )
+
+#     def _calculate_errors(
+#         self, material_parameter_corrections: Tensor, ansatz_parameters: Tensor
+#     ) -> Residuals:
+#         flattened_y_pde, flattened_y_heatflux, flattened_y_data = (
+#             self._calculate_flattened_ansatz_outputs(
+#                 material_parameter_corrections, ansatz_parameters
+#             )
+#         )
+#         errors_pde = self._error_calculator.calculate(
+#             flattened_y_pde, self._flattened_y_pde_true
+#         )
+#         errors_heatflux = self._error_calculator.calculate(
+#             flattened_y_heatflux, self._flattened_y_heatflux_true
+#         )
+#         errors_data = self._error_calculator.calculate(
+#             flattened_y_data, self._flattened_y_data_true
+#         )
+#         return errors_pde, errors_heatflux, errors_data
+
+#     def _calculate_flattened_ansatz_outputs(
+#         self, material_parameter_corrections: Tensor, ansatz_parameters: Tensor
+#     ) -> Outputs:
+#         x_param = self._material_parameters(material_parameter_corrections)
+
+#         def y_pde_func() -> Tensor:
+#             x_coor = self._data_pde.x_coor
+#             _x_param = x_param[self._parameter_indices_pde].reshape((-1, 1))
+#             heat_source = self._data_pde.f
+#             return self._pinn_funcs.pde(
+#                 self._ansatz, x_coor, _x_param, heat_source, ansatz_parameters
+#             )
+
+#         def y_heatflux_func() -> Tensor:
+#             x_coor = self._data_heatflux.x_coor
+#             _x_param = x_param[self._parameter_indices_heatflux_bc].reshape((-1, 1))
+#             normal = self._data_heatflux.normal
+#             return self._pinn_funcs.heatflux_bc(
+#                 self._ansatz, x_coor, _x_param, normal, ansatz_parameters
+#             )
+
+#         def y_data_func() -> Tensor:
+#             x_coor = self._data_calibration.inputs
+#             return self._ansatz(x_coor, ansatz_parameters)
+
+#         y_pde = flatten_tensor(y_pde_func())
+#         y_heatflux = flatten_tensor(y_heatflux_func())
+#         y_data = flatten_tensor(y_data_func())
+#         return y_pde, y_heatflux, y_data
+
+#     @abstractmethod
+#     def _split_parameters(self, parameters: Tensor) -> tuple[Tensor, ...]:
+#         raise NotImplementedError()
+
+#     def _calculate_mse_losses(self, parameters: Tensor) -> MSETuple:
+
+#         def vmap_mse_losses(parameters_sample: Tensor) -> MSETuple:
+#             (
+#                 material_parameter_corrections,
+#                 ansatz_parameters,
+#             ) = self._extract_parameters_for_mse(parameters_sample)
+#             flattened_y_pde, flattened_y_heatflux, flattened_y_data = (
+#                 self._calculate_flattened_ansatz_outputs(
+#                     material_parameter_corrections, ansatz_parameters
+#                 )
+#             )
+
+#             mse_pde = self._error_calculator.calculate_mse(
+#                 flattened_y_pde, self._flattened_y_pde_true
+#             )
+#             mse_heatflux = self._error_calculator.calculate_mse(
+#                 flattened_y_heatflux, self._flattened_y_heatflux_true
+#             )
+#             mse_data = self._error_calculator.calculate_mse(
+#                 flattened_y_data, self._flattened_y_data_true
+#             )
+#             return mse_pde, mse_heatflux, mse_data
+
+#         mses_pde, mses_heatflux, mses_data = vmap(vmap_mse_losses)(parameters)
+#         return mses_pde, mses_heatflux, mses_data
+
+#     @abstractmethod
+#     def _extract_parameters_for_mse(self, parameters: Tensor) -> tuple[Tensor, Tensor]:
+#         raise NotImplementedError()
