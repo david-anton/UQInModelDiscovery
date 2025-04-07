71acf16e0617a1716abca007a168e0d7bc2bab73
David Anton <d.anton@tu-braunschweig.de>
2025-04-07 14:07:30 +0000

    Start implementing plotting routine for Kawabata data

--- bayesianmdisc/postprocessing/plot/plot_stress.py
+++ bayesianmdisc/postprocessing/plot/plot_stress.py
@@ -88,41 +88,17 @@ def plot_stresses_linka(
     num_model_inputs = 512
 
     def split_inputs_and_outputs(
-        inputs: NPArray, outputs: NPArray, test_cases: NPArray
+        inputs: NPArray, test_cases: NPArray, outputs: NPArray
     ) -> tuple[list[NPArray], list[NPArray], list[NPArray]]:
         input_sets = np.split(inputs, num_data_sets, axis=0)
         output_sets = np.split(outputs, num_data_sets, axis=0)
         test_cases_sets = np.split(test_cases, num_data_sets, axis=0)
-        return input_sets, output_sets, test_cases_sets
-
-    def calculate_coefficient_of_determinant(
-        model: OrthotropicCANN,
-        parameter_samples: NPArray,
-        inputs: NPArray,
-        test_cases: NPArray,
-        outputs: NPArray,
-    ) -> float:
-        mean_model_outputs, _ = calculate_model_mean_and_stddev(
-            model, parameter_samples, inputs, test_cases, output_dim, device
-        )
-        return coefficient_of_determination(mean_model_outputs, outputs)
-
-    def calculate_root_mean_squared_error(
-        model: OrthotropicCANN,
-        parameter_samples: NPArray,
-        inputs: NPArray,
-        test_cases: NPArray,
-        outputs: NPArray,
-    ) -> float:
-        mean_model_outputs, _ = calculate_model_mean_and_stddev(
-            model, parameter_samples, inputs, test_cases, output_dim, device
-        )
-        return root_mean_squared_error(mean_model_outputs, outputs)
+        return input_sets, test_cases_sets, output_sets
 
     def plot_one_input_output_set(
         inputs: NPArray,
-        outputs: NPArray,
         test_cases: NPArray,
+        outputs: NPArray,
         stretch_ratio: tuple[float, float],
     ) -> None:
         ratio_fiber = stretch_ratio[index_fiber]
@@ -270,14 +246,14 @@ def plot_stresses_linka(
         figure.savefig(output_path, bbox_inches="tight", dpi=config.dpi)
         plt.clf()
 
-    input_sets, output_sets, test_cases_sets = split_inputs_and_outputs(
-        inputs, outputs, test_cases
+    input_sets, test_cases_sets, output_sets = split_inputs_and_outputs(
+        inputs, test_cases, outputs
     )
 
-    for input_set, output_set, test_case_set, stretch_ratio in zip(
+    for input_set, test_case_set, output_set, stretch_ratio in zip(
         input_sets, output_sets, test_cases_sets, stretch_ratios
     ):
-        plot_one_input_output_set(input_set, output_set, test_case_set, stretch_ratio)
+        plot_one_input_output_set(input_set, test_case_set, output_set, stretch_ratio)
 
 
 class StressPlotterConfigTreloar:
@@ -357,38 +333,49 @@ def plot_stresses_treloar(
     figure_all, axes_all = plt.subplots()
 
     def split_inputs_and_outputs(
-        inputs: NPArray, outputs: NPArray, test_cases: NPArray
-    ) -> tuple[list[NPArray], list[NPArray], list[int]]:
-        input_sets = []
-        output_sets = []
-        test_case_list = []
+        inputs: NPArray, test_cases: NPArray, outputs: NPArray
+    ) -> tuple[list[NPArray], list[int], list[NPArray]]:
 
-        for test_case in considered_test_cases:
-            filter = test_cases == test_case
-            input_sets += [inputs[filter]]
-            output_sets += [outputs[filter]]
-            test_case_list += [test_case]
+        def split_data() -> tuple[list[NPArray], list[int], list[NPArray]]:
+            input_sets = []
+            test_case_sets = []
+            output_sets = []
 
-        input_set_sizes = [len(set) for set in input_sets]
-        output_set_sizes = [len(set) for set in output_sets]
+            for test_case in considered_test_cases:
+                filter = test_cases == test_case
+                input_sets += [inputs[filter]]
+                test_case_sets += [test_case]
+                output_sets += [outputs[filter]]
 
-        valid_set_sizes = (
-            input_set_sizes == expected_set_sizes
-            and output_set_sizes == expected_set_sizes
-            and len(test_case_list) == 3
-        )
-        valid_test_case_list = test_case_list == considered_test_cases
+            return input_sets, test_case_sets, output_sets
 
-        if not valid_set_sizes and valid_test_case_list:
-            raise StressPlotterError(
-                """The number of data points to be plotted
-                                     does not match the size of the Treloar data set."""
+        def validate_data_sets(
+            input_sets: list[NPArray],
+            test_case_sets: list[int],
+            output_sets: list[NPArray],
+        ) -> None:
+            input_set_sizes = [len(set) for set in input_sets]
+            output_set_sizes = [len(set) for set in output_sets]
+
+            valid_set_sizes = (
+                input_set_sizes == expected_set_sizes
+                and output_set_sizes == expected_set_sizes
+                and len(test_case_sets) == 3
             )
+            valid_test_case_sets = test_case_sets == considered_test_cases
 
-        return input_sets, output_sets, test_case_list
+            if not valid_set_sizes and valid_test_case_sets:
+                raise StressPlotterError(
+                    """The number of data points to be plotted
+                                        does not match the size of the Treloar data set."""
+                )
+
+        input_sets, test_case_sets, output_sets = split_data()
+        validate_data_sets(input_sets, test_case_sets, output_sets)
+        return input_sets, test_case_sets, output_sets
 
     def plot_one_input_output_set(
-        inputs: NPArray, outputs: NPArray, test_case: int
+        inputs: NPArray, test_case: int, outputs: NPArray
     ) -> None:
         if test_case == test_case_identifier_uniaxial_tension:
             test_case_label = "uniaxial_tension"
@@ -607,16 +594,253 @@ def plot_stresses_treloar(
             bbox=text_properties,
         )
 
-    input_sets, output_sets, test_case_list = split_inputs_and_outputs(
+    input_sets, test_case_sets, output_sets = split_inputs_and_outputs(
         inputs, outputs, test_cases
     )
 
-    for input_set, output_set, test_case in zip(
-        input_sets, output_sets, test_case_list
+    for input_set, test_case, output_set in zip(
+        input_sets, test_case_sets, output_sets
     ):
-        plot_one_input_output_set(input_set, output_set, test_case)
+        plot_one_input_output_set(input_set, test_case, output_set)
 
     plot_all_input_and_output_sets()
+    plt.clf()
+
+
+class StressPlotterConfigKawabata:
+    def __init__(self) -> None:
+        # label size
+        self.label_size = 10
+        # font size in legend
+        self.font_size = 12
+        self.font: Dict[str, Any] = {"size": self.font_size}
+
+        # major ticks
+        self.major_tick_label_size = 12
+        self.major_ticks_size = self.font_size
+        self.major_ticks_width = 2
+
+        # minor ticks
+        self.minor_tick_label_size = 12
+        self.minor_ticks_size = 12
+        self.minor_ticks_width = 1
+
+        ### stresses
+        # color map
+        self.color_map = "viridis"
+        # data
+        self.data_label = r"$\lambda_{1}=$"
+        self.data_marker = "o"
+        self.data_marker_size = 5
+        # model
+        self.model_label = "model"
+        # standard deviation
+        self.model_stddev_alpha = 0.2
+
+        # scientific notation
+        self.scientific_notation_size = self.font_size
+
+        # save options
+        self.dpi = 300
+
+
+def plot_stresses_kawabata(
+    model: IsotropicModelLibrary,
+    parameter_samples: NPArray,
+    inputs: NPArray,
+    outputs: NPArray,
+    test_cases: NPArray,
+    output_subdirectory: str,
+    project_directory: ProjectDirectory,
+    device: Device,
+) -> None:
+    config = StressPlotterConfigKawabata()
+    num_data_points = 76
+    set_sizes = [1, 6, 6, 8, 8, 8, 8, 8, 7, 7, 6, 3]
+    num_sets = len(set_sizes)
+    output_dim = 2
+    num_model_inputs_per_set = 128
+
+    def split_inputs_and_outputs(
+        inputs: NPArray, test_cases: NPArray, outputs: NPArray
+    ) -> tuple[list[NPArray], list[NPArray], list[NPArray]]:
+
+        def validate_data() -> None:
+            num_inputs = len(inputs)
+            num_outputs = len(outputs)
+            num_test_cases = len(test_cases)
+            valid_data = (
+                num_inputs == num_data_points
+                and num_outputs == num_data_points
+                and num_test_cases == num_data_points
+            )
+
+            if not valid_data:
+                raise StressPlotterError(
+                    f"""The input and/or output do not comprise the expected number of data points 
+                    (input comprises {num_inputs} points, test cases {num_test_cases} points and 
+                    output {num_outputs} but {num_data_points} data points are expected)."""
+                )
+
+        def determine_split_indices() -> list[int]:
+            split_indices = [set_sizes[0]]
+            for i in range(1, num_sets):
+                split_indices += [split_indices[-1] + set_sizes[i]]
+            return split_indices
+
+        def split_data_sets(
+            split_indices: list[int],
+        ) -> tuple[list[NPArray], list[NPArray], list[NPArray]]:
+            input_sets = np.split(inputs, split_indices)
+            test_case_sets = np.split(test_cases, split_indices)
+            output_sets = np.split(outputs, split_indices)
+            return input_sets, test_case_sets, output_sets
+
+        validate_data()
+        split_indices = determine_split_indices()
+        return split_data_sets(split_indices)
+
+    def plot_one_input_output_set(
+        input_sets: list[NPArray],
+        output_sets: list[NPArray],
+        test_case_sets: list[NPArray],
+        stress_dim: int,
+    ) -> None:
+        file_name = f"kawabata_data_stress_{stress_dim}.png"
+
+        figure, axes = plt.subplots()
+        color_map = plt.get_cmap(config.color_map, num_sets)
+        colors = color_map(np.linspace(0, 1, num_sets))
+
+        for set_index, input_set, test_case_set, output_set in zip(
+            range(num_sets), input_sets, test_case_sets, output_sets
+        ):
+            data_stretches = input_set
+            test_case = test_case_set[0]
+            data_stresses = output_set[:, stress_dim]
+
+            data_stretch_1 = data_stretches[0, 0]
+            data_stretches_2 = data_stretches[:, 1]
+            min_stretch_2 = np.amin(data_stretches_2)
+            max_stretch_2 = np.amax(data_stretches_2)
+
+            color = colors[set_index]
+
+            # data points
+            axes.plot(
+                data_stretches_2,
+                data_stresses,
+                marker=config.data_marker,
+                color=color,
+                markersize=config.data_marker_size,
+                linestyle="None",
+                label=config.data_label + f"{data_stretch_1:.2f}",
+            )
+
+            # model
+            model_stretches_1 = np.full((num_model_inputs_per_set, 1), data_stretch_1)
+            model_stretches_2 = np.linspace(
+                min_stretch_2, max_stretch_2, num_model_inputs_per_set
+            ).reshape((-1, 1))
+            model_stretches = np.hstack((model_stretches_1, model_stretches_2))
+            model_test_cases = np.full((num_model_inputs_per_set,), test_case)
+
+            means, stddevs = calculate_model_mean_and_stddev(
+                model,
+                parameter_samples,
+                model_stretches,
+                model_test_cases,
+                output_dim,
+                device,
+            )
+            model_stretches_plot = model_stretches_2
+            means_plot = means[:, stress_dim]
+            stddevs_plot = stddevs[:, stress_dim]
+
+            axes.plot(
+                model_stretches_plot,
+                means_plot,
+                color=color,
+            )
+            axes.fill_between(
+                model_stretches_plot,
+                means_plot - stddevs_plot,
+                means_plot + stddevs_plot,
+                color=color,
+                alpha=config.model_stddev_alpha,
+            )
+
+            # axis ticks
+            x_ticks = np.linspace(min_stretch_2, max_stretch_2, num=6)
+            x_tick_labels = [str(round(tick, 2)) for tick in x_ticks]
+            axes.set_xticks(x_ticks)
+            axes.set_xticklabels(x_tick_labels)
+
+            # axis labels
+            if stress_dim == 0:
+                y_label = r"$P_{11}" + "[kPa]"
+            else:
+                y_label = r"$P_{22}" + "[kPa]"
+
+            axes.set_xlabel(r"$\lambda_{2}$" + "[-]", **config.font)
+            axes.set_ylabel(ylabel=y_label, **config.font)
+            axes.tick_params(
+                axis="both", which="minor", labelsize=config.minor_tick_label_size
+            )
+            axes.tick_params(
+                axis="both", which="major", labelsize=config.major_tick_label_size
+            )
+
+            # legend
+            axes.legend(fontsize=config.font_size, loc="outside upper right")
+
+            # text box metrics
+            r_squared = calculate_coefficient_of_determinant(
+                model,
+                parameter_samples,
+                input_set,
+                test_case_set,
+                output_set,
+                output_dim,
+                device,
+            )
+            rmse = calculate_root_mean_squared_error(
+                model,
+                parameter_samples,
+                input_set,
+                test_case_set,
+                output_set,
+                output_dim,
+                device,
+            )
+            text = "\n".join(
+                (
+                    r"$R^{2}=%.4f$" % (r_squared,),
+                    r"$RMSE=%.4f$" % (rmse,),
+                )
+            )
+            text_properties = dict(boxstyle="square", facecolor="white", alpha=1.0)
+            axes.text(
+                0.03,
+                0.97,
+                text,
+                transform=axes.transAxes,
+                fontsize=config.font_size,
+                verticalalignment="top",
+                bbox=text_properties,
+            )
+
+            output_path = project_directory.create_output_file_path(
+                file_name=file_name, subdir_name=output_subdirectory
+            )
+
+            figure.savefig(output_path, bbox_inches="tight", dpi=config.dpi)
+
+    input_sets, output_sets, test_case_sets = split_inputs_and_outputs(
+        inputs, outputs, test_cases
+    )
+    plot_one_input_output_set(input_sets, output_sets, test_case_sets, stress_dim=0)
+    plot_one_input_output_set(input_sets, output_sets, test_case_sets, stress_dim=1)
     plt.clf()
 
 
