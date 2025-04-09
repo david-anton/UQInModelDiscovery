f54023b25c4b12839bd64e35b830ef8f7d1a4a47
David Anton <d.anton@tu-braunschweig.de>
2025-04-09 08:46:13 +0000

    Fix tests

--- bayesianmdisc/models/tests/test_isotropicmodellibrary.py
+++ bayesianmdisc/models/tests/test_isotropicmodellibrary.py
@@ -7,16 +7,6 @@ device = torch.device("cpu")
 
 def get_expected_parameter_names() -> tuple[str, ...]:
 
-    def get_expected_ogden_parameter_names() -> tuple[str, ...]:
-        parameter_names = []
-        num_terms = 33
-        min_exponent = torch.tensor(-4.0)
-        max_exponent = torch.tensor(4.0)
-        exponents = torch.linspace(min_exponent, max_exponent, num_terms).tolist()
-        for index, exponent in zip(range(1, num_terms + 1), exponents):
-            parameter_names += [f"O_{index} (exponent: {round(exponent,2)})"]
-        return tuple(parameter_names)
-
     def get_expected_mr_parameter_names() -> tuple[str, ...]:
         return (
             "C_0_1 (MR)",
@@ -30,9 +20,30 @@ def get_expected_parameter_names() -> tuple[str, ...]:
             "C_3_0",
         )
 
-    ogden_parameter_names = get_expected_ogden_parameter_names()
+    def get_expected_ogden_parameter_names() -> tuple[str, ...]:
+        parameter_names = []
+        num_negative_terms = 8
+        num_positive_terms = 8
+        min_exponent = torch.tensor(-4.0)
+        max_exponent = torch.tensor(4.0)
+        negative_exponents = torch.linspace(min_exponent, 0.0, num_negative_terms + 1)[
+            :-1
+        ].tolist()
+        positive_exponents = torch.linspace(0.0, max_exponent, num_positive_terms + 1)[
+            1:
+        ].tolist()
+        exponents = negative_exponents + positive_exponents
+        for index, exponent in zip(range(1, len(exponents) + 1), exponents):
+            parameter_names += [f"O_{index} ({round(exponent,2)})"]
+        return tuple(parameter_names)
+
+    def get_expected_ln_feature_parameter_names() -> tuple[str, ...]:
+        return ("ln_I2",)
+
     mr_parameter_names = get_expected_mr_parameter_names()
-    return ogden_parameter_names + mr_parameter_names
+    ogden_parameter_names = get_expected_ogden_parameter_names()
+    ln_feature_parameter_names = get_expected_ln_feature_parameter_names()
+    return mr_parameter_names + ogden_parameter_names + ln_feature_parameter_names
 
 
 def test_get_parameter_names() -> None:
