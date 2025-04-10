8d62814919cabf6ee302e89d1d47ccce18f37450
David Anton <d.anton@tu-braunschweig.de>
2025-04-10 10:39:10 +0000

    Start implementing data splitting but still a bug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date
+from dataclasses import dataclass
 
 import torch
 
@@ -16,8 +17,12 @@ from bayesianmdisc.data import (
     StressOutputs,
     TestCases,
     TreloarDataReader,
+    test_case_identifier_pure_shear,
+    test_case_identifier_biaxial_tension,
+    test_case_identifier_equibiaxial_tension,
+    test_case_identifier_uniaxial_tension,
 )
-from bayesianmdisc.errors import DataError
+from bayesianmdisc.errors import DataError, DataSetError
 from bayesianmdisc.gppriors import infer_gp_induced_prior
 from bayesianmdisc.gps import (
     GP,
@@ -25,6 +30,7 @@ from bayesianmdisc.gps import (
     IndependentMultiOutputGP,
     create_scaled_rbf_gaussian_process,
     optimize_gp_hyperparameters,
+    condition_gp,
 )
 from bayesianmdisc.io import ProjectDirectory
 from bayesianmdisc.models import IsotropicModelLibrary, ModelProtocol, OrthotropicCANN
@@ -41,7 +47,7 @@ from bayesianmdisc.statistics.utility import (
 )
 
 data_set = "treloar"
-use_gp_prior = False
+use_gp_prior = True
 
 # Settings
 settings = Settings()
@@ -60,12 +66,7 @@ if data_set == "linka":
 elif data_set == "treloar":
     input_directory = "treloar"
     data_reader = TreloarDataReader(input_directory, project_directory, device)
-output_directory = (
-    current_date + "_" + input_directory + "_smallermodellibrary_noise_5e-2"
-)
-
-
-inputs, test_cases, outputs = data_reader.read()
+output_directory = current_date + "_" + input_directory + "_test"
 
 
 if data_set == "linka":
@@ -78,49 +79,148 @@ relative_noise_stddevs = 5e-2
 min_noise_stddev = 1e-3
 
 
-def determine_prior(
-    inputs: DeformationInputs, test_cases: TestCases, outputs: StressOutputs
-) -> PriorProtocol:
+def validate_data(
+    inputs: DeformationInputs,
+    test_cases: TestCases,
+    outputs: StressOutputs,
+    noise_stddevs: Tensor,
+) -> None:
+    num_inputs = len(inputs)
+    num_test_cases = len(test_cases)
+    num_outputs = len(outputs)
+    num_noise_stddevs = len(noise_stddevs)
 
-    def validate_number_of_samples(
-        inputs: DeformationInputs, test_cases: TestCases, outputs: StressOutputs
-    ) -> None:
-        num_inputs = len(inputs)
-        num_test_cases = len(test_cases)
-        num_outputs = len(outputs)
+    if (
+        num_inputs != num_test_cases
+        and num_inputs != num_outputs
+        and num_inputs != num_noise_stddevs
+    ):
+        raise DataError(
+            f"""The number of inputs, test cases, outputs and noise standard deviations 
+                        is expected to be the same but is {num_inputs}, {num_test_cases}, 
+                        {num_outputs} and {num_noise_stddevs}"""
+        )
 
-        if (
-            num_inputs != num_test_cases
-            or num_inputs != num_outputs
-            or num_test_cases != num_outputs
-        ):
-            raise DataError(
-                f"""The number of inputs, test cases and outputs is expected to be the same,
-                but is {num_inputs}, {num_test_cases} and {num_outputs}"""
-            )
 
-    def create_gaussian_process() -> GaussianProcess:
-        is_single_outut_gp = output_dim == 1
-        jitter = 1e-7
+def determine_heteroscedastic_noise() -> Tensor:
+    noise_stddevs = relative_noise_stddevs * outputs
+    return torch.where(
+        noise_stddevs < min_noise_stddev,
+        min_noise_stddev,
+        noise_stddevs,
+    )
 
-        def create_single_output_gp() -> GP:
-            gaussian_process = create_scaled_rbf_gaussian_process(
-                mean="zero",
-                input_dims=input_dim,
-                min_inputs=min_inputs,
-                max_inputs=max_inputs,
-                jitter=jitter,
-                device=device,
-            )
-            initial_parameters = torch.tensor(
-                [1.0] + [0.1 for _ in range(input_dim)], device=device
-            )
-            gaussian_process.set_parameters(initial_parameters)
-            return gaussian_process
 
-        def create_multi_output_gp() -> IndependentMultiOutputGP:
-            gaussian_processes = [
-                create_scaled_rbf_gaussian_process(
+@dataclass
+class SplittedData:
+    inputs_prior: DeformationInputs
+    inputs_posterior: DeformationInputs
+    test_cases_prior: TestCases
+    test_cases_posterior: TestCases
+    outputs_prior: StressOutputs
+    outputs_posterior: StressOutputs
+    noise_stddevs_prior: Tensor
+    noise_stddevs_posterior: Tensor
+
+
+def split_data(
+    inputs: DeformationInputs,
+    test_cases: TestCases,
+    outputs: StressOutputs,
+    noise_stddevs: Tensor,
+) -> SplittedData:
+
+    def split_treloar_data(
+        inputs: DeformationInputs,
+        test_cases: TestCases,
+        outputs: StressOutputs,
+        noise_stddevs: Tensor,
+    ) -> SplittedData:
+        # Number of data points
+        num_points = len(inputs)
+        mask_ut = torch.where(test_cases == test_case_identifier_uniaxial_tension)[0]
+        num_points_ut = torch.numel(mask_ut)
+        mask_ebt = torch.where(test_cases == test_case_identifier_equibiaxial_tension)[
+            0
+        ]
+        num_points_ebt = torch.numel(mask_ebt)
+
+        # Relative indices
+        rel_indices_prior_ut = [2, 6, 10, 15, 20]
+        rel_indices_prior_ebt = [2, 6, 11]
+        rel_indices_prior_ps = [2, 5, 10]
+        # Absolute indices
+        indices_prior_ut = rel_indices_prior_ut
+        start_index = num_points_ut
+        indices_prior_ebt = [i + start_index for i in rel_indices_prior_ebt]
+        start_index = num_points_ut + num_points_ebt
+        indices_prior_ps = [i + start_index for i in rel_indices_prior_ps]
+        indices_prior = indices_prior_ut + indices_prior_ebt + indices_prior_ps
+        indices_posterior = [i for i in range(num_points) if i not in indices_prior]
+
+        # Data splitting
+        inputs_prior = inputs[indices_prior, :]
+        inputs_posterior = inputs[indices_posterior, :]
+        test_cases_prior = test_cases[indices_prior]
+        test_cases_posterior = test_cases[indices_posterior]
+        outputs_prior = outputs[indices_prior, :]
+        outputs_posterior = outputs[indices_posterior, :]
+        noise_stddevs_prior = noise_stddevs[indices_prior]
+        noise_stddevs_posterior = noise_stddevs[indices_posterior]
+
+        validate_data(
+            indices_prior, test_cases_prior, outputs_prior, noise_stddevs_prior
+        )
+        validate_data(
+            indices_posterior,
+            test_cases_posterior,
+            outputs_posterior,
+            noise_stddevs_posterior,
+        )
+        return SplittedData(
+            inputs_prior=inputs_prior,
+            inputs_posterior=inputs_posterior,
+            test_cases_prior=test_cases_prior,
+            test_cases_posterior=test_cases_posterior,
+            outputs_prior=outputs_prior,
+            outputs_posterior=outputs_posterior,
+            noise_stddevs_prior=noise_stddevs_prior,
+            noise_stddevs_posterior=noise_stddevs_posterior,
+        )
+
+    validate_data(inputs, test_cases, outputs, noise_stddevs)
+    if data_set == "treloar":
+        return split_treloar_data(inputs, test_cases, outputs, noise_stddevs)
+    else:
+        raise DataSetError(f"No implementation for the requested data set {data_set}")
+
+
+inputs, test_cases, outputs = data_reader.read()
+noise_stddevs = determine_heteroscedastic_noise()
+validate_data(inputs, test_cases, outputs, noise_stddevs)
+
+
+splitted_data = split_data(inputs, test_cases, outputs, noise_stddevs)
+inputs_prior = splitted_data.inputs_prior
+inputs_posterior = splitted_data.inputs_posterior
+test_cases_prior = splitted_data.test_cases_prior
+test_cases_posterior = splitted_data.test_cases_posterior
+outputs_prior = splitted_data.outputs_prior
+outputs_posterior = splitted_data.outputs_posterior
+noise_stddevs_prior = splitted_data.noise_stddevs_prior
+noise_stddevs_posterior = splitted_data.noise_stddevs_posterior
+
+
+def determine_prior() -> PriorProtocol:
+
+    if use_gp_prior:
+
+        def create_gaussian_process() -> GaussianProcess:
+            is_single_outut_gp = output_dim == 1
+            jitter = 1e-7
+
+            def create_single_output_gp() -> GP:
+                gaussian_process = create_scaled_rbf_gaussian_process(
                     mean="zero",
                     input_dims=input_dim,
                     min_inputs=min_inputs,
@@ -128,35 +228,53 @@ def determine_prior(
                     jitter=jitter,
                     device=device,
                 )
-                for _ in range(output_dim)
-            ]
-            initial_parameters = torch.tensor(
-                [1.0] + [0.1 for _ in range(input_dim)], device=device
-            )
-
-            for gaussian_process in gaussian_processes:
+                initial_parameters = torch.tensor(
+                    [1.0] + [0.1 for _ in range(input_dim)], device=device
+                )
                 gaussian_process.set_parameters(initial_parameters)
+                return gaussian_process
 
-            return IndependentMultiOutputGP(
-                gps=tuple(gaussian_processes), device=device
-            )
+            def create_multi_output_gp() -> IndependentMultiOutputGP:
+                gaussian_processes = [
+                    create_scaled_rbf_gaussian_process(
+                        mean="zero",
+                        input_dims=input_dim,
+                        min_inputs=min_inputs,
+                        max_inputs=max_inputs,
+                        jitter=jitter,
+                        device=device,
+                    )
+                    for _ in range(output_dim)
+                ]
+                initial_parameters = torch.tensor(
+                    [1.0] + [0.1 for _ in range(input_dim)], device=device
+                )
 
-        if is_single_outut_gp:
-            return create_single_output_gp()
-        else:
-            return create_multi_output_gp()
+                for gaussian_process in gaussian_processes:
+                    gaussian_process.set_parameters(initial_parameters)
 
-    def determine_prior_moments(
-        samples: Tensor,
-    ) -> tuple[MomentsMultivariateNormal, NPArray]:
-        samples_np = samples.detach().cpu().numpy()
-        moments = determine_moments_of_multivariate_normal_distribution(samples_np)
-        return moments, samples_np
+                return IndependentMultiOutputGP(
+                    gps=tuple(gaussian_processes), device=device
+                )
 
-    validate_number_of_samples(inputs, test_cases, outputs)
-    output_subdirectory = os.path.join(output_directory, "prior")
+            if is_single_outut_gp:
+                return create_single_output_gp()
+            else:
+                return create_multi_output_gp()
 
-    if use_gp_prior:
+        def condition_gaussian_process(
+            inputs: Tensor, outputs: Tensor, noise_stddevs: Tensor
+        ) -> None:
+            condition_gp(gaussian_process, inputs, outputs, noise_stddevs, device)
+
+        def determine_prior_moments(
+            samples: Tensor,
+        ) -> tuple[MomentsMultivariateNormal, NPArray]:
+            samples_np = samples.detach().cpu().numpy()
+            moments = determine_moments_of_multivariate_normal_distribution(samples_np)
+            return moments, samples_np
+
+        output_subdirectory = os.path.join(output_directory, "prior")
         min_inputs = torch.amin(inputs, dim=0)
         max_inputs = torch.amax(inputs, dim=0)
         input_dim = inputs.size()[1]
@@ -164,37 +282,33 @@ def determine_prior(
 
         gaussian_process = create_gaussian_process()
 
-        heteroscedastic_noise_stddevs = relative_noise_stddevs * outputs
-        heteroscedastic_noise_stddevs = torch.where(
-            heteroscedastic_noise_stddevs < min_noise_stddev,
-            min_noise_stddev,
-            heteroscedastic_noise_stddevs,
-        )
-
         optimize_gp_hyperparameters(
             gaussian_process=gaussian_process,
             inputs=inputs,
             outputs=outputs,
-            initial_noise_stddevs=heteroscedastic_noise_stddevs,
-            num_iterations=int(5e4),
+            initial_noise_stddevs=noise_stddevs,
+            num_iterations=10,  # int(5e4),
             learning_rate=1e-3,
             output_subdirectory=output_subdirectory,
             project_directory=project_directory,
             device=device,
         )
 
+        condition_gaussian_process(inputs_prior, outputs_prior, noise_stddevs_prior)
+
         prior = infer_gp_induced_prior(
             gp=gaussian_process,
             model=model,
-            prior_type="Gamma",
+            prior_type="inverse Gamma",
             is_mean_trainable=True,
             inputs=inputs,
             test_cases=test_cases,
             num_func_samples=32,
             resample=True,
-            num_iters_wasserstein=int(10e3),
-            hiden_layer_size_lipschitz_nn=128,
+            num_iters_wasserstein=10,  # int(50e3),
+            hiden_layer_size_lipschitz_nn=512,
             num_iters_lipschitz=5,
+            lipschitz_func_pretraining=True,
             output_subdirectory=output_subdirectory,
             project_directory=project_directory,
             device=device,
@@ -224,15 +338,15 @@ def determine_prior(
         )
 
 
-prior = determine_prior(inputs, test_cases, outputs)
+prior = determine_prior()
 
 likelihood = Likelihood(
     model=model,
     relative_noise_stddev=relative_noise_stddevs,
     min_noise_stddev=min_noise_stddev,
-    inputs=inputs,
-    test_cases=test_cases,
-    outputs=outputs,
+    inputs=inputs_posterior,
+    test_cases=test_cases_posterior,
+    outputs=outputs_posterior,
     device=device,
 )
 
@@ -244,7 +358,7 @@ normalizing_flow_config = NormalizingFlowConfig(
     num_samples=64,
     initial_learning_rate=5e-4,
     final_learning_rate=1e-4,
-    num_iterations=100_000,
+    num_iterations=10,  # 100_000,
     deactivate_parameters=False,
     output_subdirectory=output_directory,
     project_directory=project_directory,
