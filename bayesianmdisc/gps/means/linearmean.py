87ce278c3fc2da0680f6199d26e6e485aceb3485
David Anton <d.anton@tu-braunschweig.de>
2025-05-09 07:09:30 +0000

    Add bias parameter to linear mean

--- bayesianmdisc/gps/means/linearmean.py
+++ bayesianmdisc/gps/means/linearmean.py
@@ -9,21 +9,27 @@ from bayesianmdisc.customtypes import Device, Tensor
 
 class LinearMean(NonZeroMean):
     def __init__(self, input_dim: int, device: Device) -> None:
+        self._num_weights = input_dim
+        self._num_bias = 1
+        num_hyperparameters = self._num_weights + self._num_bias
         super().__init__(
-            num_hyperparameters=input_dim,
+            num_hyperparameters=num_hyperparameters,
             device=device,
         )
-        self._mean = gpytorch.means.LinearMean(input_dim, bias=False).to(device)
+        self._mean = gpytorch.means.LinearMean(input_dim, bias=True).to(device)
 
     def forward(self, x: Tensor) -> MeanOutput:
         return self._mean(x)
 
     def set_parameters(self, parameters: Tensor) -> None:
         validate_parameters_size(parameters, self.num_hyperparameters)
-        weights_mean = parameters.clone().reshape((-1, 1))
-        self._mean.weights = torch.nn.Parameter(weights_mean).to(self._device)
+        weights = parameters[: self._num_weights].clone().reshape((-1, 1))
+        bias = parameters[-1].clone().reshape((1, 1))
+        self._mean.weights = torch.nn.Parameter(weights).to(self._device)
+        self._mean.bias = torch.nn.Parameter(bias).to(self._device)
 
     def get_named_parameters(self) -> NamedParameters:
         return {
             "weights_mean": self._mean.weights,
+            "bias_mean": self._mean.bias,
         }
