39c3e47bd49e945adbb7d6d0b15211b0353a788d
David Anton <d.anton@tu-braunschweig.de>
2025-05-13 08:43:09 +0000

    Test

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ list_relative_selection_thressholds = [1.0]
 num_samples_posterior = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_threshold_1_mae_normalizingflow_smallernf_trainablebase_lipschitznet256"
+output_directory = f"{current_date}_{input_directory}_threshold_1_mae_normalizingflow_smallernf_trainablebase_lipschitznet128"
 output_subdirectory_name_posterior = "posterior"
 output_subdirectory_name_prior = "prior"
 
@@ -275,7 +275,7 @@ if retrain_posterior:
                 num_func_samples=32,
                 resample=True,
                 num_iters_wasserstein=list_num_wasserstein_iterations[step],
-                hiden_layer_size_lipschitz_nn=256,
+                hiden_layer_size_lipschitz_nn=128,  # 256,
                 num_iters_lipschitz=5,
                 lipschitz_func_pretraining=False,
                 output_subdirectory=posterior_output_subdirectory,
