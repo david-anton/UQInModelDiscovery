da995538ca8f124ec098f5442cc4398989ae06e8
David Anton <d.anton@tu-braunschweig.de>
2025-05-15 06:01:50 +0000

    Test

--- main.py
+++ main.py
@@ -75,13 +75,13 @@ elif data_set_label == data_set_label_linka:
 relative_noise_stddevs = 5e-2
 min_noise_stddev = 1e-3
 num_calibration_steps = 2
-list_num_wasserstein_iterations = [40_000, 20_000]  # [20_000, 10_000]
+list_num_wasserstein_iterations = [20_000, 10_000]
 selection_metric = "mae"
 list_relative_selection_thressholds = [0.5]
 num_samples_posterior = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_threshold_mae_0.5_normalizingflow_lrdecay9_nflayers16_noise5e-2_largermodel"
+output_directory = f"{current_date}_{input_directory}_threshold_mae_0.5_normalizingflow_lrdecay9_nflayers16_noise5e-2_largermodel_lessiters"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_gp = "gp"
 
