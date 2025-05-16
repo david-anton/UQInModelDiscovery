f13100846eac37d9265dd09a178201b2f834c807
David Anton <d.anton@tu-braunschweig.de>
2025-05-16 12:28:26 +0000

    Test

--- main.py
+++ main.py
@@ -78,12 +78,12 @@ min_noise_stddev = 1e-3
 num_calibration_steps = 2
 list_num_wasserstein_iterations = [20_000, 10_000]
 selection_metric = "mae"
-list_relative_selection_thressholds = [0.2]
+list_relative_selection_thressholds = [0.5]
 num_samples_posterior = 4096
 
 
 output_directory = (
-    f"{current_date}_{input_directory}_threshold_mae_0.2_normalizingflow_noise5e-2"
+    f"{current_date}_{input_directory}_threshold_mae_0.5_normalizingflow_noise5e-2"
 )
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_gp = "gp"
