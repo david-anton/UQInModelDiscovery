d507010e9247bebf47538b6f615d620034d5790c
David Anton <d.anton@tu-braunschweig.de>
2025-05-18 15:19:36 +0000

    Test

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ list_relative_selection_thressholds = [0.05]
 num_samples_posterior = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_threshold_mae_0.05_normalizingflow_noise5e-2_lipschitz_iters10_lambda5_lr1_layers4_width256"
+output_directory = f"{current_date}_{input_directory}_threshold_mae_0.05_normalizingflow_noise5e-2_lipschitz_iters10_lambda2_lr1_layers4_width256"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_gp = "gp"
 
