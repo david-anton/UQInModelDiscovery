43085bc30cb1bde07243ef0f771ba203221f4e1b
David Anton <d.anton@tu-braunschweig.de>
2025-06-04 14:55:00 +0000

    Test

--- slurm/main_teaching.sh
+++ slurm/main_teaching.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu_teaching
 #SBATCH --nodes=1
-#SBATCH --time=168:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=BMDisc
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:ampere:1
