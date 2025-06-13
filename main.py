605e82e3d27fc678d626ccdc701f93f37d470de7
David Anton <d.anton@tu-braunschweig.de>
2025-06-13 14:59:09 +0000

    Test

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ elif data_set_label == data_set_label_linka:
     model = OrthotropicCANN(device)
 
     relative_noise_stddevs = 5e-2
-    min_absolute_noise_stddev = 5e-3  # 5e-2
+    min_absolute_noise_stddev = 1e-2  # 5e-2
     list_num_wasserstein_iterations = [10_000, 10_000]
     first_sobol_index_thresshold = 1e-2
 elif data_set_label == data_set_label_synthetic_linka:
