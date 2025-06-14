6c317dac96511c1cb409a34407cf5f3c8e4d2b62
David Anton <d.anton@tu-braunschweig.de>
2025-06-14 16:31:18 +0000

    Test

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ from bayesianmdisc.postprocessing.plot import (
 )
 from bayesianmdisc.settings import Settings, get_device, set_default_dtype, set_seed
 
-data_set_label = data_set_label_linka
+data_set_label = data_set_label_treloar  # data_set_label_linka
 retrain_models = True
 
 # Settings
@@ -370,8 +370,8 @@ def perform_baysian_inference_on_kawabata_data(
             relative_width_flow_layers=relative_width_flow_layers,
             num_samples=128,  # 32,
             initial_learning_rate=5e-2,  # 5e-4,
-            final_learning_rate=1e-2,  # 1e-6,
-            num_iterations=10_000,
+            final_learning_rate=1e-4,  # 1e-6,
+            num_iterations=200_000,
             output_subdirectory=output_directory,
             project_directory=project_directory,
         )
