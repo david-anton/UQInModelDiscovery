5b75dcd4f350afe0dff66165452a0cd1d815d37d
David Anton <d.anton@tu-braunschweig.de>
2025-06-17 13:30:13 +0000

    Test

--- main.py
+++ main.py
@@ -162,8 +162,8 @@ elif data_set_label == data_set_label_synthetic_linka:
 
     model = OrthotropicCANN(device)
 
-    relative_noise_stddevs = 1e-3  # 5e-2
-    min_absolute_noise_stddev = 1e-3  # 1e-2
+    relative_noise_stddevs = 5e-2
+    min_absolute_noise_stddev = 1e-2
     list_num_wasserstein_iterations = [10_000, 10_000]
     first_sobol_index_thresshold = 1e-2
 
@@ -171,7 +171,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_maternkernel"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -455,23 +455,23 @@ if retrain_models:
 
             def create_single_output_gp() -> GP:
                 gp_mean = "zero"
-                gaussian_process = create_scaled_matern_gaussian_process(
-                    mean=gp_mean,
-                    smoothness_parameter=2.5,
-                    input_dim=input_dim,
-                    min_inputs=min_inputs,
-                    max_inputs=max_inputs,
-                    jitter=jitter,
-                    device=device,
-                )
-                # gaussian_process = create_scaled_rbf_gaussian_process(
+                # gaussian_process = create_scaled_matern_gaussian_process(
                 #     mean=gp_mean,
+                #     smoothness_parameter=2.5,
                 #     input_dim=input_dim,
                 #     min_inputs=min_inputs,
                 #     max_inputs=max_inputs,
                 #     jitter=jitter,
                 #     device=device,
                 # )
+                gaussian_process = create_scaled_rbf_gaussian_process(
+                    mean=gp_mean,
+                    input_dim=input_dim,
+                    min_inputs=min_inputs,
+                    max_inputs=max_inputs,
+                    jitter=jitter,
+                    device=device,
+                )
                 initial_parameters_output_scale = [1.0]
                 initial_parameters_length_scale = [0.1 for _ in range(input_dim)]
                 initial_parameters_kernel = (
