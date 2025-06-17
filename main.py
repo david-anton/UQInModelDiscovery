83fb3aca16cd3e4cf9e6f2ac284d4cb886c2584d
David Anton <d.anton@tu-braunschweig.de>
2025-06-17 13:41:09 +0000

    Test

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_scale0.4 "
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_scale0.2"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -501,7 +501,7 @@ if retrain_models:
             num_iterations = int(1e4)
             learning_rate = 2e-1
             if data_set_label == data_set_label_synthetic_linka:
-                factor_length_scales = 0.4
+                factor_length_scales = 0.2
             else:
                 factor_length_scales = 0.8
 
