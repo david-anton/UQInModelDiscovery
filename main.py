3a84c49a362ec841815ca2a6a29028c65c48abd4
David Anton <d.anton@tu-braunschweig.de>
2025-06-20 16:01:03 +0000

    Test

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ elif data_set_label == data_set_label_linka:
     )
     num_points_per_test_case = 11
 
-    use_only_squared_anisotropic_invariants = False
+    use_only_squared_anisotropic_invariants = True
     model = OrthotropicCANN(device, use_only_squared_anisotropic_invariants)
 
     relative_noise_stddevs = 2e-2  # 5e-2
@@ -177,7 +177,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_expconstrained"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_expconstrained_reduced"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
