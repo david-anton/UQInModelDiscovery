d3f58c26dc10e010da5b760cbf5a7a16acf1635a
David Anton <d.anton@tu-braunschweig.de>
2025-06-23 06:24:20 +0000

    Test

--- main.py
+++ main.py
@@ -168,8 +168,8 @@ elif data_set_label == data_set_label_synthetic_linka:
 
     model = OrthotropicCANN(device, use_only_squared_anisotropic_invariants)
 
-    relative_noise_stddevs = 0.0  # 5e-2
-    min_absolute_noise_stddev = 1e-3  # 1e-2
+    relative_noise_stddevs = 5e-2
+    min_absolute_noise_stddev = 1e-2
     list_num_wasserstein_iterations = [10_000, 10_000]
     first_sobol_index_thresshold = 1e-2
 
@@ -177,7 +177,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_lipschitz_nn_4_512_lambda_100_kernel_matern_nf_64_8"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{first_sobol_index_thresshold}_lipschitz_nn_4_512_lambda_100_kernel_matern_nf_32_8"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
