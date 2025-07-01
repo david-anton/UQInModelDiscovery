a601a9651e82ba55add196b0c7a7555bd60dbb17
David Anton <d.anton@tu-braunschweig.de>
2025-07-01 11:06:36 +0000

    Test

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ from bayesianmdisc.postprocessing.plot import (
 from bayesianmdisc.settings import Settings, get_device, set_default_dtype, set_seed
 from bayesianmdisc.utility import from_torch_to_numpy
 
-data_set_label = data_set_label_synthetic_linka
+data_set_label = data_set_label_treloar
 retrain_models = True
 
 # Settings
@@ -96,12 +96,12 @@ if data_set_label == data_set_label_treloar:
     )
 
     model: ModelProtocol = create_isotropic_model(
-        strain_energy_function_type="library", output_dim=1, device=device
+        strain_energy_function_type="cann", output_dim=1, device=device
     )
 
-    relative_noise_stddevs = 1e-1  # 5e-2
-    min_absolute_noise_stddev = 1e-3
-    list_num_wasserstein_iterations = [20_000, 10_000]
+    relative_noise_stddevs = 1e-1
+    min_absolute_noise_stddev = 1e-2
+    list_num_wasserstein_iterations = [10_000, 10_000]
     total_sobol_index_thresshold = 1e-6
 elif data_set_label == data_set_label_linka:
     input_directory = data_set_label
@@ -161,7 +161,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_lipschitz_nn_4_512_lambda_100_iters_10_kernel_matern"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_lipschitz_nn_4_256_lambda_10_iters_10_kernel_matern_cann_scaled"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -377,6 +377,7 @@ def perform_baysian_inference_on_kawabata_data(
     else:
         load_normalizing_flow_config = LoadNormalizingFlowConfig(
             num_parameters=num_parameters,
+            parameter_scales=model.parameter_scales,
             num_flows=num_flows,
             relative_width_flow_layers=relative_width_flow_layers,
             output_subdirectory=output_directory,
