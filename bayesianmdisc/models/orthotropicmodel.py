d574ae95188e5d943ac54f821eb92bdb504c97c7
David Anton <d.anton@tu-braunschweig.de>
2025-07-03 08:41:36 +0000

    Test

--- bayesianmdisc/models/orthotropicmodel.py
+++ bayesianmdisc/models/orthotropicmodel.py
@@ -766,10 +766,10 @@ class OutputSelectorLinka:
                 selection_mask_list += _reshape(selection_mask)
             elif test_case == test_case_identifier_simple_shear_13:
                 # selection_mask[2] = True
-                selection_mask[5] = True
+                selection_mask[5] = False  # True
                 selection_mask_list += _reshape(selection_mask)
             elif test_case == test_case_identifier_simple_shear_31:
-                selection_mask[2] = True
+                selection_mask[2] = False  # True
                 # selection_mask[5] = True
                 selection_mask_list += _reshape(selection_mask)
             elif test_case == test_case_identifier_simple_shear_23:
