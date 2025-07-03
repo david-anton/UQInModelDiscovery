6b24ba473c6e7cf6f2a4ea9aaf38819a3d85bf8f
David Anton <d.anton@tu-braunschweig.de>
2025-07-03 05:52:00 +0000

    Test

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ if data_set_label == data_set_label_treloar:
         input_directory, project_directory, device
     )
 
-    strain_energy_function_type = "library"
+    strain_energy_function_type = "cann"
     model: ModelProtocol = create_isotropic_model(
         strain_energy_function_type=strain_energy_function_type,
         output_dim=1,
@@ -170,7 +170,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_lipschitz_nn_4_256_lambda_10_iters_10_kernel_rbf_0.8_library_scaled_1"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_lipschitz_nn_4_256_lambda_10_iters_10_kernel_rbf_0.8_cann_scaled_1e-3"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
