dbea997e978b32b8c9715b572f1e0b00abad378b
David Anton <d.anton@tu-braunschweig.de>
2025-07-03 12:55:40 +0000

    Test

--- main.py
+++ main.py
@@ -163,14 +163,14 @@ elif data_set_label == data_set_label_synthetic_linka:
 
     relative_noise_stddevs = 1e-1
     min_absolute_noise_stddev = 1e-2
-    list_num_wasserstein_iterations = [1_000, 1_000]
+    list_num_wasserstein_iterations = [10_000, 1_000]
     total_sobol_index_thresshold = 1e-2
 
 num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_kernel_rbf_0.8_cann_outputselection_SS"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_kernel_rbf_0.8_cann_outputselection_SS21"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
