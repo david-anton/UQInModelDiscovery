04e60006f45b6188551d210bb49c7b75302694e8
David Anton <d.anton@tu-braunschweig.de>
2025-07-07 12:28:09 +0000

    Test

--- main.py
+++ main.py
@@ -163,14 +163,14 @@ elif data_set_label == data_set_label_synthetic_linka:
 
     relative_noise_stddevs = 2e-1  # 1e-1
     min_absolute_noise_stddev = 1e-2
-    list_num_wasserstein_iterations = [10_000, 10_000]
+    list_num_wasserstein_iterations = [20_000, 10_000]
     total_sobol_index_thresshold = 1e-2
 
 num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_rbf_0.8"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_rbf_0.8_lambda_10"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -555,13 +555,13 @@ if retrain_models:
             )
 
         def extract_parameter_distribution() -> DistributionProtocol:
-            num_func_samples = 32
             num_points_per_test_case = 32
             num_iters_lipschitz = 10
             num_layers_lipschitz_nn = 4
             relative_width_lipschitz_nn = 4
 
             if data_set_label == data_set_label_treloar:
+                num_func_samples = 32
                 lipschitz_penalty_coefficient = 10.0
                 data_set_treloar = cast(TreloarDataSet, data_set)
                 inputs_extraction, test_cases_extraction = (
@@ -574,8 +574,8 @@ if retrain_models:
                 data_set_label == data_set_label_linka
                 or data_set_label == data_set_label_synthetic_linka
             ):
-
-                lipschitz_penalty_coefficient = 100.0
+                num_func_samples = 32
+                lipschitz_penalty_coefficient = 10.0
                 data_set_linka = cast(LinkaHeartDataSet, data_set)
                 inputs_extraction, test_cases_extraction = (
                     data_set_linka.generate_uniform_inputs(num_points_per_test_case)
