b3a4830635c9b6d25a09f1f7b8c6b9156c945941
David Anton <d.anton@tu-braunschweig.de>
2025-07-09 06:18:50 +0000

    Test

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_rbf_0.8_lipschitz_net_2_4_lambda_10_bothlrdecay"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_rbf_0.8_lipschitz_net_2_4_lambda_1000"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -574,7 +574,7 @@ if retrain_models:
                 data_set_label == data_set_label_linka
                 or data_set_label == data_set_label_synthetic_linka
             ):
-                lipschitz_penalty_coefficient = 10.0  # 100.0
+                lipschitz_penalty_coefficient = 1000.0  # 100.0
                 data_set_linka = cast(LinkaHeartDataSet, data_set)
                 inputs_extraction, test_cases_extraction = (
                     data_set_linka.generate_uniform_inputs(num_points_per_test_case)
