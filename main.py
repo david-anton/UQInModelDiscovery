5f12b842e80133012d551f84167442e1582b20dd
David Anton <d.anton@tu-braunschweig.de>
2025-07-14 07:02:45 +0000

    Test

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ elif data_set_label == data_set_label_linka:
 elif data_set_label == data_set_label_synthetic_linka:
     input_directory = data_set_label
     file_name = "CANNsHEARTdata_synthetic.xlsx"
-    num_points_per_test_case = 16  # 32
+    num_points_per_test_case = 11  # 16
     use_only_squared_anisotropic_invariants = True
 
     model_data_generation = OrthotropicCANN(
@@ -170,7 +170,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_rbf_0.4_lipschitz_net_2_4_lambda_100_nf_16_4"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_rbf_0.6_lipschitz_net_2_4_lambda_100_nf_16_4_noised_spectralnorm"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -503,7 +503,7 @@ if retrain_models:
             elif data_set_label == data_set_label_linka:
                 factor_length_scales = 0.8
             elif data_set_label == data_set_label_synthetic_linka:
-                factor_length_scales = 0.4
+                factor_length_scales = 0.6
 
             def optimize_hyperparameters() -> None:
                 return optimize_gp_hyperparameters(
