1d423e4e1dd70637eb3e7f0cfc4f08889b438b4e
David Anton <d.anton@tu-braunschweig.de>
2025-08-07 14:04:11 +0000

    Test

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ num_samples_parameter_distribution = 8192
 num_samples_factor_sensitivity_analysis = 4096
 
 
-output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}"
+output_directory = f"{current_date}_{input_directory}_relnoise{relative_noise_stddevs}_minnoise{min_absolute_noise_stddev}_threshold{total_sobol_index_thresshold}_noaddednoise"
 output_subdirectory_name_gp = "gp"
 output_subdirectory_name_parameters = "parameters"
 output_subdirectory_name_sensitivities = "sensitivity_analysis"
@@ -324,8 +324,8 @@ inputs, test_cases, outputs = data_set.read_data()
 noise_stddevs = determine_heteroscedastic_noise(
     relative_noise_stddevs, min_absolute_noise_stddev, outputs
 )
-if data_set_label == data_set_label_synthetic_linka:
-    outputs = add_noise_to_data(noise_stddevs, outputs, device)
+# if data_set_label == data_set_label_synthetic_linka:
+#     outputs = add_noise_to_data(noise_stddevs, outputs, device)
 
 
 validate_data(inputs, test_cases, outputs, noise_stddevs)
